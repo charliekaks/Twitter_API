09e6f440ab985459fa1a271b98d01b6ac0f17174
charliekaks <kaksunchained@gmail.com>
2018-04-18 12:13:16 +0300

    Added the logic to authenticate a user and a the appropriate route for the authentication

--- authenticator.js
+++ authenticator.js
@@ -13,6 +13,12 @@ var oauth = new OAuth(
 );
 
 module.exports = {
+    get: function(url, access_token, access_token_secret, cb) {
+		oauth.get.call(oauth, url, access_token, access_token_secret, cb);
+	},
+	post: function(url, access_token, access_token_secret, body, cb) {
+		oauth.post.call(oauth, url, access_token, access_token_secret, body, cb);
+	},
     redirectToTwitterLoginPage: function(req,res){
         //Ask twitter for a request token
         oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
@@ -35,6 +41,32 @@ module.exports = {
         // Clear the request token cookies
         res.clearCookie("oauth_token");
         res.clearCookie("oauth_token_secret");
+
+        //exchange aouth_verifier for an access token
+        oauth.getOAuthAccessToken(
+            req.cookies.oauth_token,
+            req.cookies.oauth_token_secret,
+            req.cookies.oauth_verifier,
+            function(error, oauth_access_token,oauth_access_token_secret, results){
+                if (error) {
+                    return cb(error);
+                }
+                //Get twitter user ID
+                oauth.get("https://api.twitter.com/1.1/account/verify_credentials.json",
+            oauth_access_token, oauth_access_token_secret,
+            function(error, data){
+                if (error) {
+                    return cb(error);
+                }
+                //Parse the JSON response
+                data = JSON.parse(data);
+
+                //Store the access tokens in cookies
+                res.cookie("access_token", oauth_access_token, {httpOnly: true});
+                res.cookie("access_token_secret", oauth_access_token_secret, {httpOnly:true});
+                res.cookie("twitter_id", data.id_str, {httpOnly:true});
+            }
+        )});
 
         //Tell the router authentication was successfull
         cb();
--- index.js
+++ index.js
@@ -11,6 +11,22 @@ app.use(require("cookie-parser")());
 //Take user to twitter login page
 app.get("/auth/twitter", authenticator.redirectToTwitterLoginPage);
 
+//Tweet
+app.get("/tweet", function(req, res) {
+    if (!req.cookies.access_token || !req.cookies.access_token_secret) {
+        return res.sendStatus(401);
+    }
+    authenticator.post("https://api.twitter.com/1.1/statuses/update.json",
+        req.cookies.access_token,req.cookies.access_token_secret,{
+            status: "Hello Charles Up in Here"
+        }, function(error, data) {
+            if (error) {
+                return res.status(400).send(error)
+            }
+            res.send("Tweet Successfull");
+        });
+});
+
 // The callback url that the user is redirected to after signing in
 app.get(url.parse(config.oauth_callback).path, function(req, res) {
     authenticator.authenticate(req,res, function(err) {
